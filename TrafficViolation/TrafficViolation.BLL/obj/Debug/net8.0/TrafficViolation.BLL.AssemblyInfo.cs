6215ca85a9a70e5057ce95467db6b30f01a2a2f9
linhmaideptrai <duylinh2992004@gmail.com>
2025-03-19 11:55:19 +0700

    update

--- TrafficViolation/TrafficViolation.BLL/obj/Debug/net8.0/TrafficViolation.BLL.AssemblyInfo.cs
+++ TrafficViolation/TrafficViolation.BLL/obj/Debug/net8.0/TrafficViolation.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrafficViolation.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338691d74bcbdeecc7a50e75af3b671d76f21ab7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bdb5f54b6835ddf059be564ed2581045876c222")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrafficViolation.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrafficViolation.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
